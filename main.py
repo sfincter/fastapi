a77a2c5476eb5c8778d7e198c084e2b91e15fdd6
noName <samyrize77777@gmail.com>
2025-03-02 16:03:21 +0400

    NileDB databases

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, EmailStr, field_validator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database import create_tables
+from databases import Database
 
 app = FastAPI()
 
@@ -17,20 +18,27 @@ app.add_middleware(
     allow_headers=["*"],  # Разрешаем все заголовки
 )
 
-DATABASE_URL = "postgres://01955629-3eff-7fa2-b9ec-59979ded6f4f:7cbeeccc-cde6-46e8-9f9b-b42bc7adc5f1@eu-central-1.db.thenile.dev/getbetterDB"
+DATABASE_URL = 'postgres://01955629-3eff-7fa2-b9ec-59979ded6f4f:7cbeeccc-cde6-46e8-9f9b-b42bc7adc5f1@eu-central-1.db.thenile.dev/getbetterDB'
 
-engine = create_engine(DATABASE_URL)  # Подключаемся к БД
+database = Database(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)  # Создаём сессию
 
+# Создание таблиц и другие действия с БД
 def create_tables():
     from models import Base
-    Base.metadata.create_all(bind=engine)
-
+    Base.metadata.create_all(bind=database)
 
 @app.on_event("startup")
-def startup():
-    create_tables()  # Создание таблиц при запуске
+async def startup():
+    try:
+        await database.connect()  # Подключаемся к базе данных
+        create_tables()  # Создание таблиц при запуске
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ошибка при подключении или создании таблиц: {str(e)}")
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()  # Отключаемся от базы данных
 
 @app.get("/")
 def check_db():
